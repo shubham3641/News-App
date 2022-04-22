e004bf9403dcbed060079249b173e4e88868a2bb
shubham pal <shubhampal3641@gmail.com>
2022-04-22 23:43:33 +0530

    I hukk

--- app/src/main/java/com/example/linkednews/webView.java
+++ app/src/main/java/com/example/linkednews/webView.java
@@ -1,14 +1,31 @@
 package com.example.linkednews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class webView extends AppCompatActivity {
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+public class webView extends AppCompatActivity {
+    Toolbar toolbar;
+    WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+        toolbar=findViewById(R.id.toolbar);
+        webview=findViewById(R.id.webvi);
+        setSupportActionBar(toolbar);
+
+        Intent intent=getIntent();
+        String url=intent.getStringExtra("url");
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl(url);
+
     }
+
+
 }
